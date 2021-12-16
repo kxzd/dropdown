9d6a41cedd381593723102d3f6e88c9c984074b6
kxzd <shiftyy_@hotmail.com>
2021-12-16 23:06:33 +1100

    Added event listener for navbar button

---
+++ script.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector(".navbar-button");
+const navBarLinks = document.querySelector(".navbar-links");
+
+navButton.addEventListener("click", () => {
+    navBarLinks.classList.toggle("active");
+    navButton.classList.toggle("open");
+})

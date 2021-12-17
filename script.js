5d508faf55c532a1242ac3c8e970b8331063eb8b
kxzd <shiftyy_@hotmail.com>
2021-12-17 16:40:43 +1100

    Cleaned up

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const navButton = document.querySelector(".navbar-button");
 const navBarLinks = document.querySelector(".navbar-links");
 
 navButton.addEventListener("click", () => {
-    navBarLinks.classList.toggle("active");
-    navButton.classList.toggle("open");
-})
-
+  navBarLinks.classList.toggle("active");
+  navButton.classList.toggle("open");
+});

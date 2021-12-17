8ae97dd0b28a19766e62e18cb70f431b9a52c222
kxzd <shiftyy_@hotmail.com>
2021-12-17 16:40:13 +1100

    Added example carousel page

--- carousel.js
+++ carousel.js
@@ -6,86 +6,102 @@ const dotsNav = document.querySelector(".carousel__nav");
 const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-
-// arrange slides next to one another
+// Will arrange slides to be next to eachother
 const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + "px";
-}
-
+  slide.style.left = `${slideWidth * index}px`;
+};
 slides.forEach(setSlidePosition);
 
-
 const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-    currentSlide.classList.remove("current-slide");
-    targetSlide.classList.add("current-slide")
-}
+  track.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
 
 const updateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove("current-slide");
-    targetDot.classList.add("current-slide");
-}
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
+};
 
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-    if (targetIndex === 0) {
-        prevButton.classList.add("is-hidden");
-        nextButton.classList.remove("is-hidden")
-    }
-    else if (targetIndex === slides.length - 1) {
-        prevButton.classList.remove("is-hidden");
-        nextButton.classList.add("is-hidden");
-    }
-    else {
-        prevButton.classList.remove("is-hidden");
-        nextButton.classList.remove("is-hidden");
-    }
-}
+// Function that will move the carousel to show the first slide once ned of carousel is reached
+const resetCarousel = (currentSlide, currentDot) => {
+  const firstSlide = slides[0];
+  const firstDot = dots[0];
+  moveToSlide(track, currentSlide, firstSlide);
+  updateDots(currentDot, firstDot);
+};
 
-//when i click left, move slides to left
-
-prevButton.addEventListener("click", e => {
-    const currentSlide = track.querySelector(".current-slide");
-    const previousSlide = currentSlide.previousElementSibling;
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const prevDot = currentDot.previousElementSibling;
-    const prevIndex = slides.findIndex(slide => slide === previousSlide)
-
-    updateDots(currentDot, prevDot);
-    moveToSlide(track, currentSlide, previousSlide);
-    hideShowArrows(slides, prevButton, nextButton, prevIndex);
-})
-
-// when i click right, move slides to the right
-
-nextButton.addEventListener("click", e => {
-    const currentSlide = track.querySelector(".current-slide");
-    const nextSlide = currentSlide.nextElementSibling;
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide)
-    
+const playCarousel = () => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  if (nextSlide === null) {
+    resetCarousel(currentSlide, currentDot);
+  } else {
     updateDots(currentDot, nextDot);
-    moveToSlide(track, currentSlide, nextSlide)
-    hideShowArrows(slides, prevButton, nextButton, nextIndex);
-})
+    moveToSlide(track, currentSlide, nextSlide);
+  }
+};
 
+// -----------------EVENT LISTENERS----------------//
 
-// when i click the nav indictaors move to that slide
+prevButton.addEventListener("click", () => {
+  const currentSlide = track.querySelector(".current-slide");
+  const previousSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
 
-dotsNav.addEventListener("click", e => {
-    const targetDot = e.target.closest("button");
+  if (previousSlide != null) {
+    moveToSlide(track, currentSlide, previousSlide);
+    updateDots(currentDot, prevDot);
+  } else {
+    moveToSlide(track, currentSlide, slides[slides.length - 1]);
+    updateDots(currentDot, dots[slides.length - 1]);
+  }
+});
 
-    if (!targetDot) return;
+nextButton.addEventListener("click", () => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
 
-    const currentSlide = track.querySelector(".current-slide");
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const targetIndex = dots.findIndex(dot => dot === targetDot);
-    const targetSlide = slides[targetIndex];
+  if (nextSlide != null) {
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+  } else {
+    moveToSlide(track, currentSlide, slides[0]);
+    updateDots(currentDot, dots[0]);
+  }
+});
 
-    moveToSlide(track, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot);
-    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+dotsNav.addEventListener("click", (e) => {
+  const targetDot = e.target.closest("button");
 
+  if (!targetDot) return;
 
-})
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
 
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+});
+
+// -----------------EVENT LISTENERS----------------//
+
+// Module pattern that initiates the carousel
+const carouselRepeater = (() => {
+  const player = setInterval(playCarousel, 3000);
+  nextButton.addEventListener("click", () => {
+    clearInterval(player);
+  });
+  prevButton.addEventListener("click", () => {
+    clearInterval(player);
+  });
+  dotsNav.addEventListener("click", () => {
+    clearInterval(player);
+  });
+})();

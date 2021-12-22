2083f6fad0cafb4e0766e982cc1f3f9fdc1b4038
kxzd <shiftyy_@hotmail.com>
2021-12-22 23:31:11 +1100

    Cleaned up

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const navButton = document.querySelector(".navbar-button");
 const navBarLinks = document.querySelector(".navbar-links");
 
+
 navButton.addEventListener("click", () => {
   navBarLinks.classList.toggle("active");
   navButton.classList.toggle("open");
 });
+
